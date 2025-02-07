8fa9f4c5559bb5a9cab7765169d7c515343933e3
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-02-07 20:01:03 +0900

    不要になった単体テストクラスを削除

--- Lib/Common/Tests/TestConf.cmake
+++ Lib/Common/Tests/TestConf.cmake
@@ -4,10 +4,6 @@
 ##    テストの設定。
 ##
 
-add_test(NAME   SampleDocumentTest
-    COMMAND  $<TARGET_FILE:SampleDocumentTest>
-)
-
 add_test(NAME   SampleSettingsTest
     COMMAND  $<TARGET_FILE:SampleSettingsTest>
 )
@@ -17,5 +13,4 @@ add_test(NAME   SampleSettingsTest
 ##    テストプログラムのビルド。
 ##
 
-add_executable(SampleDocumentTest       SampleDocumentTest.cpp)
 add_executable(SampleSettingsTest       SampleSettingsTest.cpp)
