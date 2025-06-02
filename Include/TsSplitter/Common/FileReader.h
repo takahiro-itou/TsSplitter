9a3782f04a221db33a49c2099cab592a60e33ad0
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-06-02 18:34:39 +0900

    空のヘッダファイルを新規作成

---
+++ Include/TsSplitter/Common/FileReader.h
@@ -0,0 +1,24 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      -- TS Splitter Project. --                      **
+**                                                                      **
+**          Copyright (C), 2025-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of FileReader class.
+**
+**      @file       Common/FileReader.h
+**/
+
+#if !defined( TSSPLITTER_COMMON_INCLUDED_FILE_READER_H )
+#    define   TSSPLITTER_COMMON_INCLUDED_FILE_READER_H
+
+#endif
