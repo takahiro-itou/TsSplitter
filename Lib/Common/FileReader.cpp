e8619d4b2c36ddd1e9179e5894f5323b1580fcb1
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-06-03 19:34:49 +0900

    空のソースファイルを新規作成

---
+++ Lib/Common/FileReader.cpp
@@ -0,0 +1,38 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      -- TS Splitter Project. --                      **
+**                                                                      **
+**          Copyright (C), 2025-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of FileReader class.
+**
+**      @file       Common/FileReader.cpp
+**/
+
+#include    "TsSplitter/Common/FileReader.h"
+
+
+TSSPLITTER_NAMESPACE_BEGIN
+namespace  Common  {
+
+namespace  {
+
+}   //  End of (Unnamed) namespace.
+
+
+//========================================================================
+//
+//    FileReader  class.
+//
+
+}   //  End of namespace  Common
+TSSPLITTER_NAMESPACE_END
