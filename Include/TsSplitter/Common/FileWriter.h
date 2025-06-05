aa6417f33e44cd1d0dfea71a169f0b30bbaab8d7
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-06-05 19:22:39 +0900

    クラスを新規作成

---
+++ Include/TsSplitter/Common/FileWriter.h
@@ -0,0 +1,127 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      -- TS Splitter Project. --                      **
+**                                                                      **
+**          Copyright (C), 2025-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of FileWriter class.
+**
+**      @file       Common/FileWriter.h
+**/
+
+#if !defined( TSSPLITTER_COMMON_INCLUDED_FILE_WRITER_H )
+#    define   TSSPLITTER_COMMON_INCLUDED_FILE_WRITER_H
+
+#if !defined( TSSPLITTER_COMMON_INCLUDED_TSSPLITTER_TYPES_H )
+#    include    "TsSplitterTypes.h"
+#endif
+
+#if !defined( TSSPLITTER_SYS_STL_INCLUDED_STRING )
+#    include    <string>
+#    define   TSSPLITTER_SYS_STL_INCLUDED_STRING
+#endif
+
+
+TSSPLITTER_NAMESPACE_BEGIN
+namespace  Common  {
+
+//========================================================================
+//
+//    FileWriter  class.
+//
+
+class  FileWriter
+{
+
+//========================================================================
+//
+//    Internal Type Definitions.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   インスタンスを初期化する
+    **  （デフォルトコンストラクタ）。
+    **
+    **/
+    FileWriter();
+
+    //----------------------------------------------------------------
+    /**   インスタンスを破棄する
+    **  （デストラクタ）。
+    **
+    **/
+    virtual  ~FileWriter();
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+//========================================================================
+//
+//    Other Features.
+//
+public:
+    //  テストクラス。  //
+    friend  class   FileWriterTest;
+};
+
+}   //  End of namespace  Common
+TSSPLITTER_NAMESPACE_END
+
+#endif
