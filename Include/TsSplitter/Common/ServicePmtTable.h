d178aa7c1a1d5e35fd4551c172fc559939d9e3eb
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-06-07 19:28:58 +0900

    空のファイルを新規作成

---
+++ Include/TsSplitter/Common/ServicePmtTable.h
@@ -0,0 +1,24 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      -- TS Splitter Project. --                      **
+**                                                                      **
+**          Copyright (C), 2025-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of ServicePmtTable class.
+**
+**      @file       Common/ServicePmtTable.h
+**/
+
+#if !defined( TSSPLITTER_COMMON_INCLUDED_SERVICE_PMT_TABLE_H )
+#    define   TSSPLITTER_COMMON_INCLUDED_SERVICE_PMT_TABLE_H
+
+#endif
---
+++ Lib/Common/ServicePmtTable.cpp
@@ -0,0 +1,21 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      -- TS Splitter Project. --                      **
+**                                                                      **
+**          Copyright (C), 2025-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of ServicePmtTable class.
+**
+**      @file       Common/ServicePmtTable.cpp
+**/
+
+#include    "TsSplitter/Common/ServicePmtTable.h"
