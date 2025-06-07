e8ca9bb306f9d4a4c6b03ff09e71a3a96a100be2
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-06-07 19:33:47 +0900

    クラスを新規作成

--- Include/TsSplitter/Common/ServicePmtTable.h
+++ Include/TsSplitter/Common/ServicePmtTable.h
@@ -21,4 +21,102 @@
 #if !defined( TSSPLITTER_COMMON_INCLUDED_SERVICE_PMT_TABLE_H )
 #    define   TSSPLITTER_COMMON_INCLUDED_SERVICE_PMT_TABLE_H
 
+#if !defined( TSSPLITTER_COMMON_INCLUDED_TSSPLITTER_TYPES_H )
+#    include    "TsSplitterTypes.h"
+#endif
+
+
+TSSPLITTER_NAMESPACE_BEGIN
+namespace  Common  {
+
+//========================================================================
+//
+//    ServicePmtTable  class.
+//
+
+class  ServicePmtTable
+{
+
+//========================================================================
+//
+//    Internal Type Definitions.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   インスタンスを初期化する
+    **  （デフォルトコンストラクタ）。
+    **
+    **/
+    ServicePmtTable();
+
+    //----------------------------------------------------------------
+    /**   インスタンスを破棄する
+    **  （デストラクタ）。
+    **
+    **/
+    virtual  ~ServicePmtTable();
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+//========================================================================
+//
+//    Other Features.
+//
+public:
+    //  テストクラス。  //
+    friend  class   ServicePmtTableTest;
+};
+
+}   //  End of namespace  Common
+TSSPLITTER_NAMESPACE_END
+
 #endif
