a7838ccdef289a3e9eef4822edae8b71c01c7809
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-06-07 23:46:28 +0900

    リファクタリング

--- Lib/Common/FileReader.cpp
+++ Lib/Common/FileReader.cpp
@@ -87,6 +87,33 @@ FileReader::parsePAT(
         const  uint8_t * p,
         int  (& pmt)[65536])
 {
+    printf("DUMP of PAT:\n");
+    for ( int y = 0; y < 188; y += 16 ) {
+        printf("%02x:", y);
+        for ( int x = 0; x < 16; ++ x ) {
+            int idx = y + x;
+            if ( idx >= 188 ) { break; }
+            printf(" %02x", p[idx]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    for ( int pg = 0; pg < 65536; ++ pg ) {
+        pmt[pg] = -1;
+    }
+
+    int secLen  = ((p[4 + 2] << 8) & 0x0F00) | (p[4 + 3] & 0x00FF);
+    int program_number;
+    int program_PMT;
+    printf(" PAT section length : %d\n", secLen);
+    for ( int idx = 13; idx < 4 + 1 + secLen - 4; idx += 4 ) {
+        program_number  = ((p[idx] << 8) & 0xFF00) | (p[idx+1] & 0x00FF);
+        program_PMT = ((p[idx+2] << 8) & 0x1F00) | (p[idx+3] & 0x00FF);
+        printf("  0x%04x(%05d), PMT: 0x%04x\n",
+               program_number, program_number, program_PMT);
+        pmt[program_number] = program_PMT;
+    }
 }
 
 int
