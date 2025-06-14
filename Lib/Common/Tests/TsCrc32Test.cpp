a5f517a4f83a998dd7a82d05c130ced78e5cf159
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-06-14 19:42:54 +0900

    単体テストケースを追加

--- Lib/Common/Tests/TsCrc32Test.cpp
+++ Lib/Common/Tests/TsCrc32Test.cpp
@@ -23,6 +23,7 @@
 
 
 TSSPLITTER_NAMESPACE_BEGIN
+namespace  Common  {
 
 //========================================================================
 //
@@ -35,7 +36,10 @@ TSSPLITTER_NAMESPACE_BEGIN
 class  TsCrc32Test : public  TestFixture
 {
     CPPUNIT_TEST_SUITE(TsCrc32Test);
-    CPPUNIT_TEST(testNameSpace);
+    CPPUNIT_TEST(testComputeCrc1);
+    CPPUNIT_TEST(testComputeCrc2);
+    CPPUNIT_TEST(testComputeCrc3);
+    CPPUNIT_TEST(testCrcConstTable);
     CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -43,7 +47,10 @@ public:
     virtual  void   tearDown()  override    { }
 
 private:
-    void  testNameSpace();
+    void  testComputeCrc1();
+    void  testComputeCrc2();
+    void  testComputeCrc3();
+    void  testCrcConstTable();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION( TsCrc32Test );
@@ -53,11 +60,49 @@ CPPUNIT_TEST_SUITE_REGISTRATION( TsCrc32Test );
 //    Tests.
 //
 
-void  TsCrc32Test::testNameSpace()
+void  TsCrc32Test::testComputeCrc1()
 {
+    BtByte  data[] = {
+        0x00
+    };
+
+    TsCrc32::CrcVal crc = TsCrc32::computeCrc32(data, getArraySize(data));
+    CPPUNIT_ASSERT_EQUAL(0x4E08BFB4u, crc);
+
     return;
 }
 
+void  TsCrc32Test::testComputeCrc2()
+{
+    BtByte  data[] = {
+        0xFE
+    };
+
+    TsCrc32::CrcVal crc = TsCrc32::computeCrc32(data, getArraySize(data));
+    CPPUNIT_ASSERT_EQUAL(0xFB3EE2B7u, crc);
+
+    return;
+}
+
+void  TsCrc32Test::testComputeCrc3()
+{
+    BtByte  data[] = {
+        0xFF
+    };
+
+    TsCrc32::CrcVal crc = TsCrc32::computeCrc32(data, getArraySize(data));
+    CPPUNIT_ASSERT_EQUAL(0xFFFFFF00u, crc);
+
+    return;
+}
+
+void  TsCrc32Test::testCrcConstTable()
+{
+    //  今はまだテーブルが無いので何もしない。  //
+    return;
+}
+
+}   //  End of namespace  Common
 TSSPLITTER_NAMESPACE_END
 
 //========================================================================
