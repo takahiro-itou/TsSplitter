af87ad74470d538b6a30fc998d388cec56c84945
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-06-14 18:55:23 +0900

    空のファイルを新規作成

---
+++ Include/TsSplitter/Common/TsCrc32.h
@@ -0,0 +1,24 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      -- TS Splitter Project. --                      **
+**                                                                      **
+**          Copyright (C), 2025-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of TsCrc32 class.
+**
+**      @file       Common/TsCrc32.h
+**/
+
+#if !defined( TSSPLITTER_COMMON_INCLUDED_TS_CRC32_H )
+#    define   TSSPLITTER_COMMON_INCLUDED_TS_CRC32_H
+
+#endif
