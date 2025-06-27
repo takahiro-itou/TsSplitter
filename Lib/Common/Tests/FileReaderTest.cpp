1f6e9ef8ab511cfaecd1b90b5e598ed394002105
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-06-27 21:39:47 +0900

    単体テストクラスを新規作成

---
+++ Lib/Common/Tests/FileReaderTest.cpp
@@ -0,0 +1,74 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      -- TS Splitter Project. --                      **
+**                                                                      **
+**          Copyright (C), 2025-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of Test Case 'FileReader'.
+**
+**      @file       Common/Tests/FileReaderTest.cpp
+**/
+
+#include    "TestDriver.h"
+#include    "TsSplitter/Common/FileReader.h"
+
+
+TSSPLITTER_NAMESPACE_BEGIN
+namespace  Common  {
+
+//========================================================================
+//
+//    FileReaderTest  class.
+//
+/**
+**    クラス FileReader  の単体テスト。
+**/
+
+class  FileReaderTest : public  TestFixture
+{
+    CPPUNIT_TEST_SUITE(FileReaderTest);
+    CPPUNIT_TEST(testFileReader);
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    virtual  void   setUp()     override    { }
+    virtual  void   tearDown()  override    { }
+
+private:
+    void  testFileReader();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION( FileReaderTest );
+
+//========================================================================
+//
+//    Tests.
+//
+
+void  FileReaderTest::testFileReader()
+{
+    FileReader  testee;
+    return;
+}
+
+}   //  End of namespace  Common
+TSSPLITTER_NAMESPACE_END
+
+//========================================================================
+//
+//    エントリポイント。
+//
+
+int  main(int argc, char * argv[])
+{
+    return ( executeCppUnitTests(argc, argv) );
+}
