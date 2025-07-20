564a65d5384c6c26fd9dc16fee965a47b47c032c
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-07-20 18:12:29 +0900

    空のソースファイルを新規作成

---
+++ Bin/ExtractPID.cpp
@@ -0,0 +1,37 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      -- TS Splitter Project. --                      **
+**                                                                      **
+**          Copyright (C), 2025-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      サンプルプログラム。
+**
+**      @file       Bin/ExtractPID.cpp
+**/
+
+#include    "TsSplitter/Common/TsSplitterTypes.h"
+#include    "TsSplitter/Common/FileReader.h"
+
+#include    <cstdint>
+#include    <cstring>
+#include    <iostream>
+
+using   namespace   TSSPLITTER_NAMESPACE;
+
+int  main(int argc, char * argv[])
+{
+    if ( argc >= 2 ) {
+        fr.parseTsFile(argv[1]);
+    }
+
+    return ( 0 );
+}
