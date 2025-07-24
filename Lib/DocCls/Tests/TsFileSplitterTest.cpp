558a421316fea3004ebf492a65d4f716a4f6f446
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-07-24 18:54:38 +0900

    空のテストを新規作成

---
+++ Lib/DocCls/Tests/TsFileSplitterTest.cpp
@@ -0,0 +1,33 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      -- TS Splitter Project. --                      **
+**                                                                      **
+**          Copyright (C), 2025-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of Test Case 'TsFileSplitter'.
+**
+**      @file       Common/Tests/TsFileSplitterTest.cpp
+**/
+
+#include    "TestDriver.h"
+#include    "TsSplitter/DocCls/TsFileSplitter.h"
+
+
+//========================================================================
+//
+//    エントリポイント。
+//
+
+int  main(int argc, char * argv[])
+{
+    return ( executeCppUnitTests(argc, argv) );
+}
