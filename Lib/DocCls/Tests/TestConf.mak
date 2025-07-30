e5ceb0158e8109950c091c396b979b05f7f0dabc
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-07-30 22:06:24 +0900

    単体テストを追加

--- Lib/DocCls/Tests/TestConf.mak
+++ Lib/DocCls/Tests/TestConf.mak
@@ -12,7 +12,7 @@ TESTS                   =  \
 ##    Test Configurations.
 ##
 
-target_testee_library       +=  -lsampleCommon
+target_testee_library       +=  -lsampleDocCls
 
 dist_noinst_data_files      +=
 dist_noinst_header_files    +=
--- Lib/DocCls/Tests/TsFileSplitterTest.cpp
+++ Lib/DocCls/Tests/TsFileSplitterTest.cpp
@@ -15,12 +15,56 @@
 /**
 **      An Implementation of Test Case 'TsFileSplitter'.
 **
-**      @file       Common/Tests/TsFileSplitterTest.cpp
+**      @file       DocCls/Tests/TsFileSplitterTest.cpp
 **/
 
 #include    "TestDriver.h"
 #include    "TsSplitter/DocCls/TsFileSplitter.h"
 
+
+TSSPLITTER_NAMESPACE_BEGIN
+namespace  DocCls  {
+
+//========================================================================
+//
+//    TsFileSplitterTest  class.
+//
+/**
+**    クラス TsFileSplitter  の単体テスト。
+**/
+
+class  TsFileSplitterTest : public  TestFixture
+{
+    CPPUNIT_TEST_SUITE(TsFileSplitterTest);
+    CPPUNIT_TEST(testTsFileSplitter);
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    virtual  void   setUp()     override    { }
+    virtual  void   tearDown()  override    { }
+
+private:
+    void  testTsFileSplitter();
+
+private:
+    typedef     TsFileSplitter  Testee;
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION( TsFileSplitterTest );
+
+//========================================================================
+//
+//    Tests.
+//
+
+void  TsFileSplitterTest::testTsFileSplitter()
+{
+    Testee  testee;
+    return;
+}
+
+}   //  End of namespace  DocCls
+TSSPLITTER_NAMESPACE_END
 
 //========================================================================
 //
